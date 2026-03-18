1f1aaa0af465921a1874d5df39bc3300293c2599
darweshmasizo-lgtm <darweshmasizo@gmail.com>
2026-03-18 22:49:05 +0300

    add setInterval

--- script.js
+++ script.js
@@ -1,2 +1,12 @@
-const icons = document.querySelectorAll(".section-1-icons i")
+const icons = document.querySelectorAll('.section-1-icons i')
 
+
+setInterval(() =>{
+
+  const icon = document.querySelector('.section-1-icons .change')
+
+ icon.classList.remove('change')
+ icon.nextElementSibling.classList.add('change')
+
+
+}, 4000)
