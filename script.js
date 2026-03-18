24a67ec502d1d9601e864bfe2336bd8ad7b09040
darweshmasizo-lgtm <darweshmasizo@gmail.com>
2026-03-18 22:58:28 +0300

    add if

--- script.js
+++ script.js
@@ -1,9 +1,15 @@
 const icons = document.querySelectorAll('.section-1-icons i')
-
+let1=1
 
 setInterval(() =>{
+    i++
 
   const icon = document.querySelector('.section-1-icons .change')
+
+   if(i > icons.length){
+      icon[0].classList.add('change')
+      i = 1
+   }
 
  icon.classList.remove('change')
  icon.nextElementSibling.classList.add('change')
